8d6937a6815d21dc5521676351bf1c0ff1ae50bb
farocan <anna.fr.1990@gmail.com>
2024-02-11 17:39:49 +0100

    fix styling and refresh routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import 'bootstrap';
@@ -9,11 +9,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
